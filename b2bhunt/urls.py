9f07137546cea09da4df49c4787df6d1a27e0a72
Saurav <mdsauravchowdhury@gamil.com>
2020-09-27 00:13:06 -0700

    add ckeditor upload path

--- b2bhunt/urls.py
+++ b2bhunt/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base_app.urls'))
+    path('', include('base_app.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls'))
 ]
